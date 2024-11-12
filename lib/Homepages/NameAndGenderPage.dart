406921043f447c13bedfe6ea2e558d0baed02959
Piratha <piratha250392@yahoo.com>
2024-11-12 10:22:23 +0530

    ok

--- lib/Homepages/NameAndGenderPage.dart
+++ lib/Homepages/NameAndGenderPage.dart
@@ -12,21 +12,23 @@ class NameAndGender extends StatefulWidget {
 }
 
 class _NameAndGenderState extends State<NameAndGender> {
+
+
   String Gender = "";
+  void Changed(String gen) {
+    setState(() {
+      Gender = "Male";
+    });
+  }
 
   void onPressed() {
     Navigator.push(context,
     MaterialPageRoute(builder: (context)=>const ChartOptions()));
   }
 
-  void Changed() {
-    setState(() {
-      Gender = "Male";
-    });
-  }
-
   @override
   Widget build(BuildContext context) {
+
     return Scaffold(
       backgroundColor: MyMateThemes.backgroundColor,
       appBar: AppBar(
@@ -38,82 +40,24 @@ class _NameAndGenderState extends State<NameAndGender> {
             Form(
                 child: Column(
               children: [
-                SizedBox(
-                  width: 300, // Set the width of the TextField
-                  height: 50, // Set the height of the TextField
-                  child: TextField(
-                    decoration: MyInputDecorations("First name"),
-                    style: TextStyle(
-                      fontSize: 25,
-                      fontWeight: FontWeight.w500
-                  ),
-                  ),
-                ),
+                  InputField("First name"),
                 Container( height: 50,),
-                SizedBox(
-                  width: 300, // Set the width of the TextField
-                  height: 50, // Set the height of the TextField
-                  child: TextField(
-                    style: TextStyle(
-                      fontSize: 25,
-                      fontWeight: FontWeight.w500
-                    ),
-                    decoration: MyInputDecorations("Last name"),
-                  ),
-                ),
-                Container(
-                  height: 50,
-                ),
+                  InputField("Last name"),
+                Container( height: 50,),
                 Row(
                   mainAxisAlignment: MainAxisAlignment.center,
                   children: [
-                    ElevatedButton.icon(
-                      onPressed: Changed,
-                      label: const Text(""),
-                      icon: Icon(Icons.male_rounded),
-                      style: const ButtonStyle(
-                        foregroundColor: MaterialStatePropertyAll(Colors.indigo),
-                        alignment: Alignment.center,
-                        textStyle: MaterialStatePropertyAll(TextStyle(
-                          fontWeight: FontWeight.w800
-                        )),
-                        iconSize: MaterialStatePropertyAll(70),
-                        shape: MaterialStatePropertyAll(RoundedRectangleBorder()),
-                        fixedSize: MaterialStatePropertyAll(Size(120, 120))
-                      ),
-                    ),
+                    GenderButton("Male",Icons.male_rounded),
                     SizedBox(width: 50,),
-                    ElevatedButton.icon(
-                      onPressed: Changed,
-                      label: const Text(""),
-                      icon: Icon(Icons.female_outlined),
-                      style: const ButtonStyle(
-                       foregroundColor: MaterialStatePropertyAll(Colors.purple),
-                          alignment: Alignment.center,
-                          textStyle: MaterialStatePropertyAll(TextStyle(
-                              fontWeight: FontWeight.w800,
-
-                          )),
-                          iconSize: MaterialStatePropertyAll(70),
-                          shape: MaterialStatePropertyAll(RoundedRectangleBorder()),
-                          fixedSize: MaterialStatePropertyAll(Size(120, 120))
-                      ),
-                    ),
+                    GenderButton("Fe-Male",Icons.female_outlined),
                   ],
-
                 ),
                 const SizedBox(height: 65,),
                 SizedBox(
                   height: 58,
                   width: 166,
                   child: ElevatedButton(onPressed: onPressed,
-                    style: const ButtonStyle(
-                      foregroundColor: MaterialStatePropertyAll(Colors.white),
-                      backgroundColor:
-                      MaterialStatePropertyAll(MyMateThemes.primaryColor),
-                      shape: MaterialStatePropertyAll(RoundedRectangleBorder(
-                          borderRadius: BorderRadius.zero)),
-                    ),
+                    style: CommonButtonStyle.commonButtonStyle(),
                     child: const Text(
                       "Next",
                       style: TextStyle(fontSize: MyMateThemes.buttonFontSize),
@@ -121,22 +65,51 @@ class _NameAndGenderState extends State<NameAndGender> {
                   ),
                 ),
               ],
-            ))
+            ),
+            )
           ],
         ),
       ),
     );
   }
+
 }
 
-InputDecoration MyInputDecorations(String hint){
-  return InputDecoration(
+ElevatedButton GenderButton(String gender,IconData icon){
 
-    hintText: hint,
-    hintStyle: const TextStyle(
-      fontWeight: FontWeight.w600,
-      fontSize: 25,
+  return ElevatedButton.icon(
+    onPressed: (){
+     _NameAndGenderState().Changed(gender);
+    },
+    label: const Text(""),
+    icon: Icon(icon),
+    style: const ButtonStyle(
+        foregroundColor: MaterialStatePropertyAll(Colors.indigo),
+        alignment: Alignment.center,
+        textStyle: MaterialStatePropertyAll(TextStyle(
+            fontWeight: FontWeight.w800
+        )),
+        iconSize: MaterialStatePropertyAll(70),
+        shape: MaterialStatePropertyAll(RoundedRectangleBorder()),
+        fixedSize: MaterialStatePropertyAll(Size(120, 120))
     ),
-
   );
 }
+
+SizedBox InputField(String inputName){
+  return SizedBox(
+    width: 300, // Set the width of the TextField
+    height: 50, // Set the height of the TextField
+    child: TextField(
+      decoration: InputDecoration(
+          hintText: inputName,
+          hintStyle: const TextStyle(
+            fontWeight: FontWeight.w600,
+            fontSize: 25,
+          ),
+    ),
+  ),
+  );
+}
+
+
