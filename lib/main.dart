cf2e78bebd126c331d2521a0f3d99818339f5ac0
Thinusan <Thinusan@example.com>
2024-11-20 14:35:40 +0530

    New commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:mymateapp/Homepages/AddTokenPage.dart';
 import 'package:mymateapp/Homepages/CompleteProfileScreen/CompleteProfileMain.dart';
 import 'package:mymateapp/Homepages/ListViewPage.dart';
 import 'package:mymateapp/Homepages/NameAndGenderPage.dart';
@@ -26,7 +27,7 @@ class MyApp extends StatelessWidget {
           useMaterial3: true,
           primaryColor: Colors.blue[200]),
       debugShowCheckedModeBanner: false,
-      home: CompleteProfilePage(),
+      home: AddTokenPage(),
     );
   }
 }
