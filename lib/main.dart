e45163fe012c13fce9541d510a04ada9c5a2d6e1
Srishayuchja <srishayi04@gmail.com>
2024-12-03 10:40:19 +0530

    test

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:mymateapp/dbConnection/Clients.dart';
 import 'Homepages/Profiles/EditPage.dart';
+import 'Homepages/explorePage/explorePageMain.dart';
 import 'firebase_options.dart';
 
 void main() async {
@@ -29,7 +30,7 @@ class MyApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       home:
       //OtpPinput(docId: "9l2knrHe8XLZL2S3erxy"),
-      EditPage(),
+      Explorepage(),
     );
   }
 }
